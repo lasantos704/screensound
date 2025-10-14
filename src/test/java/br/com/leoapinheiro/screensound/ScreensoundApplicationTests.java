94bed8d6902cb6a8f887e8b0e45bdd6393753c18
Leonardo Pinheiro <lasantos704@gmail.com>
2025-10-14 08:38:51 -0300

    first commit

---
+++ src/test/java/br/com/leoapinheiro/screensound/ScreensoundApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leoapinheiro.screensound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreensoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
